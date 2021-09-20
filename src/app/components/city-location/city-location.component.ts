5adbc2105aa427aed98abb76b87c87086e2a79a6
Yurii <y.nadilnyi@gmail.com>
2021-09-20 14:06:03 +0300

    map style  added

--- src/app/components/city-location/city-location.component.ts
+++ src/app/components/city-location/city-location.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } fr
 import { Loader } from '@googlemaps/js-api-loader';
 import { UserCoordinates } from 'src/app/objects-types/user-coordinates';
 import { SharingWeatherDataService } from 'src/app/services/sharing-weather-data.service';
+import { mapStyle } from './map-style';
+// import { mapStyle } from 'src/app/components/city-location/map-style.ts'
 @Component({
 	selector: 'app-city-location',
 	templateUrl: './city-location.component.html',
@@ -34,150 +36,7 @@ export class CityLocationComponent implements OnInit, AfterViewInit {
 					zoom: 9,
 					disableDefaultUI: true,
 					draggable: false,
-					styles: [
-						{
-							'stylers': [
-								{
-									'saturation': -45
-								},
-								{
-									'lightness': 13
-								}
-							]
-						},
-						{
-							'featureType': 'road.highway',
-							'elementType': 'geometry.fill',
-							'stylers': [
-								{
-									'color': '#8fa7b3'
-								}
-							]
-						},
-						{
-							'featureType': 'road.highway',
-							'elementType': 'geometry.stroke',
-							'stylers': [
-								{
-									'color': '#667780'
-								}
-							]
-						},
-						{
-							'featureType': 'road.highway',
-							'elementType': 'labels.text.fill',
-							'stylers': [
-								{
-									'color': '#333333'
-								}
-							]
-						},
-						{
-							'featureType': 'road.highway',
-							'elementType': 'labels.text.stroke',
-							'stylers': [
-								{
-									'color': '#8fa7b3'
-								},
-								{
-									'gamma': 2
-								}
-							]
-						},
-						{
-							'featureType': 'road.arterial',
-							'elementType': 'geometry.fill',
-							'stylers': [
-								{
-									'color': '#a3becc'
-								}
-							]
-						},
-						{
-							'featureType': 'road.arterial',
-							'elementType': 'geometry.stroke',
-							'stylers': [
-								{
-									'color': '#7a8f99'
-								}
-							]
-						},
-						{
-							'featureType': 'road.arterial',
-							'elementType': 'labels.text.fill',
-							'stylers': [
-								{
-									'color': '#555555'
-								}
-							]
-						},
-						{
-							'featureType': 'road.local',
-							'elementType': 'geometry.fill',
-							'stylers': [
-								{
-									'color': '#a3becc'
-								}
-							]
-						},
-						{
-							'featureType': 'road.local',
-							'elementType': 'geometry.stroke',
-							'stylers': [
-								{
-									'color': '#7a8f99'
-								}
-							]
-						},
-						{
-							'featureType': 'road.local',
-							'elementType': 'labels.text.fill',
-							'stylers': [
-								{
-									'color': '#555555'
-								}
-							]
-						},
-						{
-							'featureType': 'water',
-							'elementType': 'geometry.fill',
-							'stylers': [
-								{
-									'color': '#bbd9e9'
-								}
-							]
-						},
-						{
-							'featureType': 'administrative',
-							'elementType': 'labels.text.fill',
-							'stylers': [
-								{
-									'color': '#525f66'
-								}
-							]
-						},
-						{
-							'featureType': 'transit',
-							'elementType': 'labels.text.stroke',
-							'stylers': [
-								{
-									'color': '#bbd9e9'
-								},
-								{
-									'gamma': 2
-								}
-							]
-						},
-						{
-							'featureType': 'transit.line',
-							'elementType': 'geometry.fill',
-							'stylers': [
-								{
-									'color': '#a3aeb5'
-								}
-							]
-						}
-					]
+					styles: mapStyle,
 				} as google.maps.MapOptions)
 			})
 		}, 0)
