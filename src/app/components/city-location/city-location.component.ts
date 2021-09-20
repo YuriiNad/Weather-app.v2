962ace11eef63592e1eba6a379d4ca21ca269c1b
Yurii <y.nadilnyi@gmail.com>
2021-09-20 16:59:27 +0300

    extra data removed (city location)

--- src/app/components/city-location/city-location.component.ts
+++ src/app/components/city-location/city-location.component.ts
@@ -1,17 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
-import { UserCoordinates } from 'src/app/objects-types/user-coordinates';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MapService } from 'src/app/services/map.service';
 import { SharingWeatherDataService } from 'src/app/services/sharing-weather-data.service';
-import { mapStyle } from './map-style';
-// import { mapStyle } from 'src/app/components/city-location/map-style.ts'
 @Component({
 	selector: 'app-city-location',
 	templateUrl: './city-location.component.html',
 	styleUrls: ['./city-location.component.scss']
 })
 export class CityLocationComponent implements OnInit, AfterViewInit {
-	private weatherAPIkey = 'AIzaSyCawDz-TKGdLqMk-N7zZKrZRJxpfZKU32k'
 	public isHidden = true;
 	public cityMap!: HTMLElement;
 
