8e32469b9f0521ebb48bad2b38543a642c507f17
Yurii <y.nadilnyi@gmail.com>
2021-09-22 19:39:22 +0300

    social media links

--- src/app/components/footer/social-media-sharing/social-media-sharing.component.ts
+++ src/app/components/footer/social-media-sharing/social-media-sharing.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-social-media-sharing',
-  templateUrl: './social-media-sharing.component.html',
-  styleUrls: ['./social-media-sharing.component.scss']
+	selector: 'app-social-media-sharing',
+	templateUrl: './social-media-sharing.component.html',
+	styleUrls: ['./social-media-sharing.component.scss']
 })
 export class SocialMediaSharingComponent implements OnInit {
-  constructor() { }
+	constructor(private _router: Router) { }
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+	}
+	goToSocial(url: string): void {
+		this._router.navigateByUrl(url)
+	}
 }
